cf1407ad2ec5f4c0a7a9e95a2329addf168d4433
KC <krkeco@gmail.com>
2019-02-17 23:33:32 -0800

    content-type

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Users from '@/components/Users'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Users',
+      component: Users
     }
   ]
 })
